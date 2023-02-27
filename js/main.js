2cbab6904c801f07a77e5e79d9467e3b1d39f52c
AynurSalimli <aynur.selimli@ascca.edu.az>
2023-02-27 15:09:08 +0400

    slider

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
 let latestSlide = slides.length - 1;
 
+slides.forEach((slide, index) => {
+  let calculatePercent = 100 * (index - currentSlide);
+  slide.style.transform = `translateX(${calculatePercent}%)`;
+});
+
 nextBtn.addEventListener("click", () => {
   if (currentSlide === latestSlide) {
     currentSlide = 0;
@@ -13,8 +18,8 @@ nextBtn.addEventListener("click", () => {
   }
 
   slides.forEach((slide, index) => {
-    let calculatePercant = 100 * (index - currentSlide);
-    slide.style.transform = `translateX(${calculatePercant}%)`;
+    let calculatePercent = 100 * (index - currentSlide);
+    slide.style.transform = `translateX(${calculatePercent}%)`;
   });
 });
 
@@ -26,7 +31,125 @@ prevBtn.addEventListener("click", () => {
   }
 
   slides.forEach((slide, index) => {
-    let calculatePercant = 100 * (index - currentSlide);
-    slide.style.transform = `translateX(${calculatePercant}%)`;
+    let calculatePercent = 100 * (index - currentSlide);
+    slide.style.transform = `translateX(${calculatePercent}%)`;
   });
 });
+
+// second slider
+
+const secondPrevBtn = document.querySelector(".second--btn-prev");
+const secondNextBtn = document.querySelector(".second--btn-next");
+const secondSlides = document.querySelectorAll(".second--slide");
+
+let secondCurrentSlide = 0;
+let secondLatestSlide = secondSlides.length - 1;
+
+secondSlides.forEach((slide, index) => {
+  let calculatePercent = 100 * index;
+  slide.style.transform = `translateX(${calculatePercent}%)`;
+});
+
+secondNextBtn.addEventListener("click", () => {
+  if (secondCurrentSlide === secondLatestSlide) {
+    secondCurrentSlide = 0;
+  } else {
+    secondCurrentSlide++;
+  }
+
+  secondSlides.forEach((slide, index) => {
+    let calculatePercent = 100 * (index - secondCurrentSlide);
+    slide.style.transform = `translateX(${calculatePercent}%)`;
+  });
+});
+
+secondPrevBtn.addEventListener("click", () => {
+  if (secondCurrentSlide === 0) {
+    secondCurrentSlide = secondLatestSlide;
+  } else {
+    secondCurrentSlide--;
+  }
+
+  secondSlides.forEach((slide, index) => {
+    let calculatePercent = 100 * (index - secondCurrentSlide);
+    slide.style.transform = `translateX(${calculatePercent}%)`;
+  });
+});
+
+// third slider
+
+const thirdPrevBtn = document.querySelector(".third-btn-prev");
+const thirdNextBtn = document.querySelector(".third-btn-next");
+const thirdSlides = document.querySelectorAll(".third--slide");
+const thirdDots = document.querySelector(".dots");
+
+let thirdCurrentSlide = 0;
+let thirdLatestSlide = thirdSlides.length - 1;
+
+thirdSlides.forEach((slide, index) => {
+  let dot = document.createElement("div");
+  dot.classList.add("dot");
+  if (index == thirdCurrentSlide) {
+    dot.classList.add("active");
+  }
+  thirdDots.appendChild(dot);
+
+  let calculatePercent = 100 * index;
+  slide.style.transform = `translateX(${calculatePercent}%)`;
+});
+
+thirdNextBtn.addEventListener("click", () => {
+  if (thirdCurrentSlide === thirdLatestSlide) {
+    thirdCurrentSlide = 0;
+  } else {
+    thirdCurrentSlide++;
+  }
+
+  thirdSlides.forEach((slide, index) => {
+    let calculatePercent = 100 * (index - thirdCurrentSlide);
+    slide.style.transform = `translateX(${calculatePercent}%)`;
+  });
+  changeActive();
+});
+
+thirdPrevBtn.addEventListener("click", () => {
+  if (thirdCurrentSlide === 0) {
+    thirdCurrentSlide = thirdLatestSlide;
+  } else {
+    thirdCurrentSlide--;
+  }
+
+  thirdSlides.forEach((slide, index) => {
+    let calculatePercent = 100 * (index - thirdCurrentSlide);
+    slide.style.transform = `translateX(${calculatePercent}%)`;
+  });
+  changeActive();
+});
+
+function changeActive() {
+  document.querySelectorAll(".dot").forEach((dot, index) => {
+    dot.classList.remove("active");
+
+    if (index == thirdCurrentSlide) {
+      dot.classList.add("active");
+    }
+  });
+}
+
+function changeSlidesWithDots() {
+  document.querySelectorAll(".dot").forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      document
+        .querySelectorAll(".dot")
+        .forEach((dot) => dot.classList.remove("active"));
+      dot.classList.add("active");
+      thirdCurrentSlide = index;
+      thirdSlides.forEach((slide, index) => {
+        let calculatePercent = 100 * (index - thirdCurrentSlide);
+        slide.style.transform = `translateX(${calculatePercent}%)`;
+      });
+    });
+  });
+}
+
+changeSlidesWithDots();
